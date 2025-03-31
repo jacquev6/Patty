8b918faa13f6b91788334a6f4c9853334f66a267
Vincent Jacques <vincent@vincent-jacques.net>
2025-03-31 09:26:34 +0200

    Use OpenAI by default in production (#3)

--- backend/patty/asgi.py
+++ backend/patty/asgi.py
@@ -1,3 +1,5 @@
+import os
+
 import fastapi
 
 from . import adaptation
@@ -9,16 +11,24 @@ app = fastapi.FastAPI()
 
 @app.get("/api/available-llm-models")
 def get_available_llm_models() -> list[llm.ConcreteModel]:
-    # @todo Hide DummyModel in production
-    return [
-        llm.DummyModel(name="dummy-1"),
-        llm.DummyModel(name="dummy-2"),
-        llm.DummyModel(name="dummy-3"),
-        llm.MistralAiModel(name="mistral-large-2411"),
-        llm.MistralAiModel(name="mistral-small-2501"),
-        llm.OpenAiModel(name="gpt-4o-2024-08-06"),
-        llm.OpenAiModel(name="gpt-4o-mini-2024-07-18"),
-    ]
+    if os.environ.get("PATTY_ENVIRONMENT") == "dev":
+        return [
+            llm.DummyModel(name="dummy-1"),
+            llm.DummyModel(name="dummy-2"),
+            llm.OpenAiModel(name="gpt-4o-2024-08-06"),
+            llm.OpenAiModel(name="gpt-4o-mini-2024-07-18"),
+            llm.MistralAiModel(name="mistral-large-2411"),
+            llm.MistralAiModel(name="mistral-small-2501"),
+        ]
+    else:
+        return [
+            llm.OpenAiModel(name="gpt-4o-2024-08-06"),
+            llm.OpenAiModel(name="gpt-4o-mini-2024-07-18"),
+            llm.MistralAiModel(name="mistral-large-2411"),
+            llm.MistralAiModel(name="mistral-small-2501"),
+            llm.DummyModel(name="dummy-1"),
+            llm.DummyModel(name="dummy-2"),
+        ]
 
 
 app.include_router(adaptation.router, prefix="/api/adaptation")
