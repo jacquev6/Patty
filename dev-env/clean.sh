2e7965a562fde076e0257f40571e00e85e33e0b9
Vincent Jacques <vincent@vincent-jacques.net>
2025-04-01 16:09:12 +0200

    Add scripts for repetitive commands

---
+++ dev-env/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+
+git clean $@ -xd .venv backend frontend dev-env/backend/pip-packages dev-env/frontend/cache
+
+git clean -nxd | sed 's/^Would remove/Kept/'
