1c366a15cf6d13679af17ef7db2502cfee6b0ce2
Vincent Jacques <vincent@vincent-jacques.net>
2025-04-16 15:34:53 +0200

    Reformat end-to-end test files

--- frontend/e2e-tests/adaptation.cy.ts
+++ frontend/e2e-tests/adaptation.cy.ts
@@ -4,7 +4,7 @@ describe('The batch creation page', () => {
 
     cy.request('POST', 'http://fixtures-loader/load?fixtures=dummy-adaptation')
 
-    Cypress.on('uncaught:exception', error => {
+    Cypress.on('uncaught:exception', (error) => {
       if (error.message.includes('ResizeObserver loop completed with undelivered notifications.')) {
         // @todo Deep dive into this issue: avoid the error instead of ignoring it.
         // https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver#observation_errors
@@ -16,7 +16,7 @@ describe('The batch creation page', () => {
 
     cy.visit('/new-batch')
 
-    cy.get('[data-cy="identified-user"]').type('Alice', {delay: 0})
+    cy.get('[data-cy="identified-user"]').type('Alice', { delay: 0 })
     cy.get('[data-cy="identified-user-ok"]').click()
   })
 
@@ -29,12 +29,17 @@ describe('The batch creation page', () => {
 
     cy.get('button:contains("Submit")').should('be.enabled')
     cy.get('@input-text').should('have.length', 2)
-    cy.get('@input-text').eq(0).should('have.value', '5 Complète avec "le vent" ou "la pluie"\na. Les feuilles sont chahutées par ...\nb. Les vitres sont mouillées par ...\n')
-    cy.get('@input-text').eq(1).should('have.value', '').type('Blah blah blah.', {delay: 0})
+    cy.get('@input-text')
+      .eq(0)
+      .should(
+        'have.value',
+        '5 Complète avec "le vent" ou "la pluie"\na. Les feuilles sont chahutées par ...\nb. Les vitres sont mouillées par ...\n',
+      )
+    cy.get('@input-text').eq(1).should('have.value', '').type('Blah blah blah.', { delay: 0 })
     cy.get('@input-text').should('have.length', 3)
-    cy.get('@input-text').eq(2).should('have.value', '').type('Bleh bleh bleh.', {delay: 0})
+    cy.get('@input-text').eq(2).should('have.value', '').type('Bleh bleh bleh.', { delay: 0 })
     cy.get('@input-text').should('have.length', 4)
-    cy.get('@input-text').eq(3).should('have.value', '').type('Blih blih blih.', {delay: 0})
+    cy.get('@input-text').eq(3).should('have.value', '').type('Blih blih blih.', { delay: 0 })
     cy.get('@input-text').should('have.length', 5)
     cy.get('@input-text').eq(3).type('{selectAll}{del}')
     cy.get('@input-text').should('have.length', 4)
@@ -65,7 +70,7 @@ describe('The batch creation page', () => {
   it('remembers the last system prompt used', () => {
     cy.get('[data-cy="system-prompt"]').as('system-prompt')
 
-    cy.get('@system-prompt').type(' Blih blih.', {delay: 0})
+    cy.get('@system-prompt').type(' Blih blih.', { delay: 0 })
     cy.get('@system-prompt').should('have.value', 'Blah blah blah. Blih blih.')
     cy.get('button:contains("Submit")').click()
     cy.get('p:contains("Created by: Alice")').should('exist')
@@ -138,8 +143,8 @@ describe('The batch creation page', () => {
     cy.get('[data-cy="input-text"]').as('input-text')
 
     cy.get('@input-text').should('have.length', 2)
-    cy.get('@input-text').eq(0).type('{selectAll}Blah blah.', {delay: 0})
-    cy.get('@input-text').eq(1).type('{selectAll}Bleh bleh.', {delay: 0})
+    cy.get('@input-text').eq(0).type('{selectAll}Blah blah.', { delay: 0 })
+    cy.get('@input-text').eq(1).type('{selectAll}Bleh bleh.', { delay: 0 })
     cy.get('button:contains("Submit")').click()
     cy.get('p:contains("Created by: Alice")').should('exist')
 
@@ -154,37 +159,47 @@ describe('The batch creation page', () => {
     cy.get('[data-cy="input-text"]').as('input-text')
 
     cy.get('@system-prompt').should('have.value', 'Blah blah blah.')
-    cy.get('@input-text').eq(0).should('have.value', '5 Complète avec "le vent" ou "la pluie"\na. Les feuilles sont chahutées par ...\nb. Les vitres sont mouillées par ...\n')
-    cy.get('@system-prompt').type('{selectAll}Alice\'s prompt.', {delay: 0})
-    cy.get('@input-text').eq(0).type('{selectAll}Alice\'s input.', {delay: 0})
+    cy.get('@input-text')
+      .eq(0)
+      .should(
+        'have.value',
+        '5 Complète avec "le vent" ou "la pluie"\na. Les feuilles sont chahutées par ...\nb. Les vitres sont mouillées par ...\n',
+      )
+    cy.get('@system-prompt').type("{selectAll}Alice's prompt.", { delay: 0 })
+    cy.get('@input-text').eq(0).type("{selectAll}Alice's input.", { delay: 0 })
     cy.get('button:contains("Submit")').click()
     cy.get('p:contains("Created by: Alice")').should('exist')
 
     cy.visit('/new-batch')
-    cy.get('@system-prompt').should('have.value', 'Alice\'s prompt.')
-    cy.get('@input-text').eq(0).should('have.value', 'Alice\'s input.')
+    cy.get('@system-prompt').should('have.value', "Alice's prompt.")
+    cy.get('@input-text').eq(0).should('have.value', "Alice's input.")
     cy.get('[data-cy="edit-identified-user"]').click()
-    cy.get('[data-cy="identified-user"]').type('{selectAll}Bob', {delay: 0})
+    cy.get('[data-cy="identified-user"]').type('{selectAll}Bob', { delay: 0 })
     cy.get('[data-cy="identified-user-ok"]').click()
     cy.get('@system-prompt').should('have.value', 'Blah blah blah.')
-    cy.get('@input-text').eq(0).should('have.value', '5 Complète avec "le vent" ou "la pluie"\na. Les feuilles sont chahutées par ...\nb. Les vitres sont mouillées par ...\n')
-    cy.get('@system-prompt').type('{selectAll}Bob\'s prompt.', {delay: 0})
-    cy.get('@input-text').eq(0).type('{selectAll}Bob\'s input.', {delay: 0})
+    cy.get('@input-text')
+      .eq(0)
+      .should(
+        'have.value',
+        '5 Complète avec "le vent" ou "la pluie"\na. Les feuilles sont chahutées par ...\nb. Les vitres sont mouillées par ...\n',
+      )
+    cy.get('@system-prompt').type("{selectAll}Bob's prompt.", { delay: 0 })
+    cy.get('@input-text').eq(0).type("{selectAll}Bob's input.", { delay: 0 })
     cy.get('button:contains("Submit")').click()
     cy.get('p:contains("Created by: Bob")').should('exist')
 
     cy.visit('/new-batch')
-    cy.get('@system-prompt').should('have.value', 'Bob\'s prompt.')
-    cy.get('@input-text').eq(0).should('have.value', 'Bob\'s input.')
+    cy.get('@system-prompt').should('have.value', "Bob's prompt.")
+    cy.get('@input-text').eq(0).should('have.value', "Bob's input.")
     cy.get('[data-cy="edit-identified-user"]').click()
-    cy.get('[data-cy="identified-user"]').type('{selectAll}Alice', {delay: 0})
+    cy.get('[data-cy="identified-user"]').type('{selectAll}Alice', { delay: 0 })
     cy.get('[data-cy="identified-user-ok"]').click()
-    cy.get('@system-prompt').should('have.value', 'Alice\'s prompt.')
-    cy.get('@input-text').eq(0).should('have.value', 'Alice\'s input.')
+    cy.get('@system-prompt').should('have.value', "Alice's prompt.")
+    cy.get('@input-text').eq(0).should('have.value', "Alice's input.")
   })
 
   it('submits a batch and refreshes until it is ready', () => {
-    cy.get('[data-cy="input-text"]').eq(0).type('{selectAll}Sleep 2', {delay: 0})
+    cy.get('[data-cy="input-text"]').eq(0).type('{selectAll}Sleep 2', { delay: 0 })
 
     cy.get('button:contains("Submit")').click()
     cy.get('p:contains("Created by: Alice")').should('exist')
@@ -193,7 +208,7 @@ describe('The batch creation page', () => {
   })
 
   it('handles non-JSON response from the LLM', () => {
-    cy.get('[data-cy="input-text"]').eq(0).type('{selectAll}Not JSON', {delay: 0})
+    cy.get('[data-cy="input-text"]').eq(0).type('{selectAll}Not JSON', { delay: 0 })
 
     cy.get('button:contains("Submit")').click()
 
@@ -202,7 +217,7 @@ describe('The batch creation page', () => {
   })
 
   it('handles invalid JSON response from the LLM', () => {
-    cy.get('[data-cy="input-text"]').eq(0).type('{selectAll}Invalid JSON', {delay: 0})
+    cy.get('[data-cy="input-text"]').eq(0).type('{selectAll}Invalid JSON', { delay: 0 })
 
     cy.get('button:contains("Submit")').click()
 
@@ -211,14 +226,13 @@ describe('The batch creation page', () => {
   })
 })
 
-
 describe('The batch edition page', () => {
   beforeEach(() => {
     cy.viewport(1600, 800)
 
     cy.request('POST', 'http://fixtures-loader/load?fixtures=mixed-dummy-batch')
 
-    Cypress.on('uncaught:exception', error => {
+    Cypress.on('uncaught:exception', (error) => {
       if (error.message.includes('ResizeObserver loop completed with undelivered notifications.')) {
         // @todo Deep dive into this issue: avoid the error instead of ignoring it.
         // https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver#observation_errors
@@ -244,7 +258,10 @@ describe('The batch edition page', () => {
   it('looks like this', () => {
     cy.visit('/batch-1')
 
-    cy.compareSnapshot({name: `batch-edition-page.1.${Cypress.browser.name}`, cypressScreenshotOptions: {disableTimersAndAnimations: true}})
+    cy.compareSnapshot({
+      name: `batch-edition-page.1.${Cypress.browser.name}`,
+      cypressScreenshotOptions: { disableTimersAndAnimations: true },
+    })
 
     cy.get('button:contains("Full screen")').eq(0).click()
 
@@ -258,12 +275,11 @@ describe('The batch edition page', () => {
     cy.get('[data-cy="multipleChoicesInput"]').eq(0).click()
     cy.get('[data-cy="choice0"]').click()
     cy.get('[data-cy="multipleChoicesInput"]').eq(0).should('contain', 'vent')
-    
+
     cy.visit('/batch-1')
     cy.get('[data-cy="multipleChoicesInput"]').eq(0).should('not.contain', 'vent')
   })
 })
-
 
 describe('The adaptation edition page', () => {
   beforeEach(() => {
@@ -271,7 +287,7 @@ describe('The adaptation edition page', () => {
 
     cy.request('POST', 'http://fixtures-loader/load?fixtures=mixed-dummy-batch')
 
-    Cypress.on('uncaught:exception', error => {
+    Cypress.on('uncaught:exception', (error) => {
       if (error.message.includes('ResizeObserver loop completed with undelivered notifications.')) {
         // @todo Deep dive into this issue: avoid the error instead of ignoring it.
         // https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver#observation_errors
@@ -323,10 +339,10 @@ describe('The adaptation edition page', () => {
 
     cy.get('@user-prompt').should('have.value', '')
     cy.get('@submit-adjustment').should('be.disabled')
-    cy.get('@user-prompt').type('Blah.', {delay: 0})
+    cy.get('@user-prompt').type('Blah.', { delay: 0 })
     cy.get('@user-prompt').should('have.value', 'Blah.')
     cy.get('@submit-adjustment').should('be.enabled')
-    cy.get('@user-prompt').type('{selectAll}{del}', {delay: 0})
+    cy.get('@user-prompt').type('{selectAll}{del}', { delay: 0 })
     cy.get('@user-prompt').should('have.value', '')
     cy.get('@submit-adjustment').should('be.disabled')
   })
@@ -337,12 +353,12 @@ describe('The adaptation edition page', () => {
     cy.get('[data-cy="user-prompt"]').as('user-prompt')
     cy.get('[data-cy="submit-adjustment"]').as('submit-adjustment')
 
-    cy.get('@user-prompt').type('Blah.', {delay: 0})
+    cy.get('@user-prompt').type('Blah.', { delay: 0 })
     cy.get('@submit-adjustment').click()
     cy.get('@user-prompt').should('have.value', '')
     cy.get('div.user-prompt:contains("Blah.")').should('exist')
     cy.get('div.user-prompt:contains("Blah.")').find('div:contains("❌")').should('exist')
-    cy.get('@user-prompt').type('Bleh.', {delay: 0})
+    cy.get('@user-prompt').type('Bleh.', { delay: 0 })
     cy.get('@submit-adjustment').click()
     cy.get('@user-prompt').should('have.value', '')
     cy.get('div.user-prompt:contains("Blah.")').should('exist')
@@ -355,15 +371,17 @@ describe('The adaptation edition page', () => {
     cy.get('div.user-prompt:contains("Blah.")').should('not.exist')
   })
 
-  const unformattedJson = '{"format":"v1","instruction":{"lines":[{"contents":[{"kind":"text","text":"Blah"}]}]},"example":null,"hint":null,"statement":{"pages":[]},"reference":null}'
-  const formattedJson = '{\n  "format": "v1",\n  "instruction": {"lines": [{"contents": [{"kind": "text", "text": "Blah"}]}]},\n  "example": null,\n  "hint": null,\n  "statement": {"pages": []},\n  "reference": null\n}'
+  const unformattedJson =
+    '{"format":"v1","instruction":{"lines":[{"contents":[{"kind":"text","text":"Blah"}]}]},"example":null,"hint":null,"statement":{"pages":[]},"reference":null}'
+  const formattedJson =
+    '{\n  "format": "v1",\n  "instruction": {"lines": [{"contents": [{"kind": "text", "text": "Blah"}]}]},\n  "example": null,\n  "hint": null,\n  "statement": {"pages": []},\n  "reference": null\n}'
 
   it('reformats manual edits', () => {
     cy.visit('/adaptation-1')
 
     cy.get('[data-cy="manual-edition"]').as('manual-edition')
 
-    cy.get('@manual-edition').type('{selectAll}').type(unformattedJson, {delay: 0, parseSpecialCharSequences: false})
+    cy.get('@manual-edition').type('{selectAll}').type(unformattedJson, { delay: 0, parseSpecialCharSequences: false })
     cy.get('@manual-edition').should('have.value', unformattedJson)
     cy.get('button[data-cy="reformat-manual-edition"]').click()
     cy.get('@manual-edition').should('have.value', formattedJson)
@@ -374,7 +392,7 @@ describe('The adaptation edition page', () => {
 
     cy.get('[data-cy="manual-edition"]').as('manual-edition')
 
-    cy.get('@manual-edition').type('{selectAll}').type(unformattedJson, {delay: 0, parseSpecialCharSequences: false})
+    cy.get('@manual-edition').type('{selectAll}').type(unformattedJson, { delay: 0, parseSpecialCharSequences: false })
 
     cy.visit('/adaptation-1')
 
@@ -390,7 +408,7 @@ describe('The adaptation edition page', () => {
 
     cy.get('@user-prompt').should('be.enabled').type('Adjust!')
     cy.get('@submit-adjustment').should('be.enabled')
-    cy.get('@manual-edition').type('Blih blih.', {delay: 0})
+    cy.get('@manual-edition').type('Blih blih.', { delay: 0 })
     cy.get('@user-prompt').should('be.disabled')
     cy.get('@submit-adjustment').should('be.disabled')
     cy.get('[data-cy="reset-manual-edition"]').click()
@@ -401,7 +419,7 @@ describe('The adaptation edition page', () => {
   it('handles non-JSON response from the LLM and rewinds it', () => {
     cy.visit('/adaptation-1')
 
-    cy.get('[data-cy="user-prompt"]').type('{selectAll}Not JSON', {delay: 0})
+    cy.get('[data-cy="user-prompt"]').type('{selectAll}Not JSON', { delay: 0 })
 
     cy.get('[data-cy="submit-adjustment"]').click()
 
@@ -418,7 +436,7 @@ describe('The adaptation edition page', () => {
   it('handles invalid JSON response from the LLM', () => {
     cy.visit('/adaptation-1')
 
-    cy.get('[data-cy="user-prompt"]').type('{selectAll}Invalid JSON', {delay: 0})
+    cy.get('[data-cy="user-prompt"]').type('{selectAll}Invalid JSON', { delay: 0 })
 
     cy.get('[data-cy="submit-adjustment"]').click()
 
@@ -439,7 +457,7 @@ describe('The adaptation edition page', () => {
     cy.get('[data-cy="multipleChoicesInput"]').eq(0).click()
     cy.get('[data-cy="choice0"]').click()
     cy.get('[data-cy="multipleChoicesInput"]').eq(0).should('contain', 'vent')
-    
+
     cy.visit('/adaptation-1')
     cy.get('[data-cy="multipleChoicesInput"]').eq(0).should('not.contain', 'vent')
   })
