be48a5f6796ba5702d3f3274b429bb1e41ab57f4
Vincent Jacques <vincent@vincent-jacques.net>
2025-04-16 16:07:46 +0200

    Disable spellcheck during tests (avoid spurious visual diffs)

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -5,7 +5,8 @@ import getCompareSnapshotsPlugin from 'cypress-image-diff-js/plugin'
 function setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
   config = getCompareSnapshotsPlugin(on, config)
 
-  // Inspired by the contents of 'getCompareSnapshotsPlugin' but fixed for browser.name === 'chromium'
+  // Enable high-resolution screenshots (inspired by the contents of 'getCompareSnapshotsPlugin' but fixed for browser.name === 'chromium')
+  // Disable spellcheck
   on('before:browser:launch', function (browser, launchOptions) {
     const width = 1600
     const height = 1200
@@ -13,16 +14,19 @@ function setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigO
     if (browser.name === 'chromium') {
       launchOptions.args.push(`--window-size=${width},${height}`)
       launchOptions.args.push('--force-device-scale-factor=1')
+      launchOptions.preferences.default['browser.enable_spellchecking'] = false
     }
 
     if (browser.name === 'electron') {
       launchOptions.preferences.width = width
       launchOptions.preferences.height = height
+      launchOptions.preferences.webPreferences.spellcheck = false
     }
 
     if (browser.name === 'firefox') {
       launchOptions.args.push(`--width=${width}`)
       launchOptions.args.push(`--height=${height}`)
+      launchOptions.preferences['layout.spellcheckDefault'] = 0
     }
 
     return launchOptions
