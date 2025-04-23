5c81760bd34d079255a7a93a32d8c5116cd4f4bf
Vincent Jacques <vincent@vincent-jacques.net>
2025-04-23 16:47:27 +0200

    Add data migration script (#29)

--- dev-env/backend/patch-last-migration.sh
+++ dev-env/backend/patch-last-migration.sh
@@ -7,7 +7,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 set -x
 
-backup_to_load=s3://jacquev6/patty/prod/backups/patty-backup-20250422-062406.tar.gz
+backup_to_load=s3://jacquev6/patty/prod/backups/patty-backup-20250423-132405.tar.gz
 
 ./shell.sh -c "python -m patty restore-database --yes --patch-according-to-settings $backup_to_load"
 ./alembic.sh upgrade 429d2fb463dd  # To be removed: only here because we have two migrations for next release. (Won't hurt until removed: will be no-op)
