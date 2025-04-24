85207437acc4a6c9cad8500e39cce781be5c6cda
Vincent Jacques <vincent@vincent-jacques.net>
2025-04-24 16:12:14 +0200

    Build Docker images for arm64

--- prod/build.sh
+++ prod/build.sh
@@ -10,22 +10,35 @@ patty_version=$1
 test -n "$patty_version"
 
 action=$2
-test "$action" == "push" -o "$action" == "load"
+if [ "$action" == "push" ]
+then
+  platform=linux/amd64,linux/arm64
+  push_load=--push
+elif [ "$action" == "load" ]
+then
+  platform=linux/amd64
+  push_load=--load
+else
+  exit 1
+fi
 
 ../dev-env/clean.sh -f
+
+if ! docker buildx ls | grep patty-multi-platform-builder >/dev/null
+then
+  docker buildx create --name patty-multi-platform-builder
+fi
 
 for part in $(grep 'AS final-' docker/Dockerfile | sed 's/.*AS final-//')
 do
   echo $part
   echo $part | sed 's/./-/g'
-  docker build \
+  docker buildx build \
     --pull \
+    --builder patty-multi-platform-builder \
     .. --file docker/Dockerfile --target final-$part \
     --build-arg PATTY_VERSION=$patty_version \
-    --tag jacquev6/patty:$patty_version-$part
-
-  if [ "$action" == "push" ]
-  then
-    docker push jacquev6/patty:$patty_version-$part
-  fi
+    --tag jacquev6/patty:$patty_version-$part \
+    --platform $platform \
+    $push_load
 done
