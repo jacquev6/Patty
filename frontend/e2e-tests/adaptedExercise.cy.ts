8029536e4519ca643d65284572efc4252329f54a
Vincent Jacques <vincent@vincent-jacques.net>
2025-04-24 11:02:41 +0200

    Add test showing the problem with #31

--- frontend/e2e-tests/adaptedExercise.cy.ts
+++ frontend/e2e-tests/adaptedExercise.cy.ts
@@ -14,6 +14,144 @@ describe('The autonomous HTML for a single adaptation', () => {
     cy.visit('/api/adaptation/export/adaptation-1.html?download=false')
     cy.get('[data-cy="multipleChoicesInput"]').eq(0).should('contain', 'vent')
   })
+
+  it('forgets student answers when the exercise is modified', () => {
+    cy.visit('/api/adaptation/export/adaptation-1.html?download=false')
+    cy.get('[data-cy="multipleChoicesInput"]').eq(0).should('not.contain', 'vent')
+    cy.get('[data-cy="multipleChoicesInput"]').eq(0).click()
+    cy.get('[data-cy="choice0"]').click()
+    cy.get('[data-cy="multipleChoicesInput"]').eq(0).should('contain', 'vent')
+
+    cy.visit('/adaptation-1')
+    cy.get('[data-cy="manual-edition"]')
+      .type('{selectall}{backspace}', { delay: 0 })
+      .type(
+        `{
+          "format": "v1",
+          "instruction": {
+            "lines": [
+              {
+                "contents": [
+                  {"kind": "text", "text": "Complète"},
+                  {"kind": "whitespace"},
+                  {"kind": "text", "text": "avec"},
+                  {"kind": "whitespace"},
+                  {
+                    "kind": "choice",
+                    "contents": [
+                      {"kind": "text", "text": "le"},
+                      {"kind": "whitespace"},
+                      {"kind": "text", "text": "vent"}
+                    ]
+                  },
+                  {"kind": "whitespace"},
+                  {"kind": "text", "text": "ou"},
+                  {"kind": "whitespace"},
+                  {
+                    "kind": "choice",
+                    "contents": [
+                      {"kind": "text", "text": "la"},
+                      {"kind": "whitespace"},
+                      {"kind": "text", "text": "pluie"}
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          "example": null,
+          "hint": null,
+          "statement": {
+            "pages": [
+              {
+                "lines": [
+                  {
+                    "contents": [
+                      {"kind": "text", "text": "a"},
+                      {"kind": "text", "text": "."},
+                      {"kind": "whitespace"},
+                      {"kind": "text", "text": "Les"},
+                      {"kind": "whitespace"},
+                      {"kind": "text", "text": "feuilles"},
+                      {"kind": "whitespace"},
+                      {"kind": "text", "text": "sont"},
+                      {"kind": "whitespace"},
+                      {"kind": "text", "text": "chahutées"},
+                      {"kind": "whitespace"},
+                      {"kind": "text", "text": "par"},
+                      {"kind": "whitespace"},
+                      {
+                        "kind": "multipleChoicesInput",
+                        "choices": [
+                          {
+                            "contents": [
+                              {"kind": "text", "text": "le"},
+                              {"kind": "whitespace"},
+                              {"kind": "text", "text": "VENT"}
+                            ]
+                          },
+                          {
+                            "contents": [
+                              {"kind": "text", "text": "la"},
+                              {"kind": "whitespace"},
+                              {"kind": "text", "text": "pluie"}
+                            ]
+                          }
+                        ],
+                        "showChoicesByDefault": false
+                      }
+                    ]
+                  },
+                  {
+                    "contents": [
+                      {"kind": "text", "text": "b"},
+                      {"kind": "text", "text": "."},
+                      {"kind": "whitespace"},
+                      {"kind": "text", "text": "Les"},
+                      {"kind": "whitespace"},
+                      {"kind": "text", "text": "vitres"},
+                      {"kind": "whitespace"},
+                      {"kind": "text", "text": "sont"},
+                      {"kind": "whitespace"},
+                      {"kind": "text", "text": "mouillées"},
+                      {"kind": "whitespace"},
+                      {"kind": "text", "text": "par"},
+                      {"kind": "whitespace"},
+                      {
+                        "kind": "multipleChoicesInput",
+                        "choices": [
+                          {
+                            "contents": [
+                              {"kind": "text", "text": "le"},
+                              {"kind": "whitespace"},
+                              {"kind": "text", "text": "vent"}
+                            ]
+                          },
+                          {
+                            "contents": [
+                              {"kind": "text", "text": "la"},
+                              {"kind": "whitespace"},
+                              {"kind": "text", "text": "pluie"}
+                            ]
+                          }
+                        ],
+                        "showChoicesByDefault": false
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          "reference": null
+        }`,
+        { delay: 0, parseSpecialCharSequences: false },
+      )
+    cy.wait(500)
+
+    cy.visit('/api/adaptation/export/adaptation-1.html?download=false')
+    cy.get('[data-cy="multipleChoicesInput"]').eq(0).should('contain', 'VENT') // @todo(#31) It should contain '....' instead of 'VENT'
+  })
 })
 
 describe('The autonomous HTML for a batch', () => {
