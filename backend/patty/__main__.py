34682a307977ae92cf78641545ea524de34aa884
Vincent Jacques <vincent@vincent-jacques.net>
2025-04-28 06:51:00 +0200

    Reformat after d655a1cf

--- backend/patty/__main__.py
+++ backend/patty/__main__.py
@@ -129,7 +129,10 @@ def backup_database() -> None:
     else:
         raise NotImplementedError(f"Unsupported database backup URL scheme: {parsed_database_backups_url.scheme}")
 
-    requests.post(settings.DATABASE_BACKUP_PULSE_MONITORING_URL, json={"archive_url": f"{settings.DATABASE_BACKUPS_URL}/{archive_name}"})
+    requests.post(
+        settings.DATABASE_BACKUP_PULSE_MONITORING_URL,
+        json={"archive_url": f"{settings.DATABASE_BACKUPS_URL}/{archive_name}"},
+    )
     print(
         f"Backed up database {settings.DATABASE_URL} to {settings.DATABASE_BACKUPS_URL}/{archive_name}", file=sys.stderr
     )
