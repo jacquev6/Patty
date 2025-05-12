ae0efb2ae357fb35c60ab3baa4e260b0177a3a2e
Vincent Jacques <vincent@vincent-jacques.net>
2025-05-12 10:28:06 +0200

    Scale up investigation for #39

--- dev-env/investigate-issue-39.sh
+++ dev-env/investigate-issue-39.sh
@@ -8,7 +8,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # Outputs of actual runs of this script are in comments in https://github.com/jacquev6/Patty/issues/39
 reference_batch_id=46
-count=7
+count=50
 
 ./docker-compose.sh exec backend-shell python -m patty restore-database --yes --patch-according-to-settings s3://jacquev6/patty/prod/backups/patty-backup-20250512-051611.tar.gz
 ./docker-compose.sh exec --workdir /app/backend/patty backend-shell alembic upgrade head
