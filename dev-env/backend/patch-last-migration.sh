dd53d08984904b9a7e88427bc0c20c3be0e58ced
Vincent Jacques <vincent@vincent-jacques.net>
2025-05-12 08:39:06 +0200

    Load a more recent backup

--- dev-env/backend/patch-last-migration.sh
+++ dev-env/backend/patch-last-migration.sh
@@ -7,7 +7,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 set -x
 
-backup_to_load=s3://jacquev6/patty/prod/backups/patty-backup-20250505-051611.tar.gz
+backup_to_load=s3://jacquev6/patty/prod/backups/patty-backup-20250512-051611.tar.gz
 
 ./shell.sh -c "python -m patty restore-database --yes --patch-according-to-settings $backup_to_load"
 
