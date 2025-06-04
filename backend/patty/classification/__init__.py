7515084eef8a4754bb290c9ed2963d2e332d8f25
Vincent Jacques <vincent@vincent-jacques.net>
2025-06-04 08:49:43 +0200

    Remove unused commented code (#49)

--- backend/patty/classification/__init__.py
+++ backend/patty/classification/__init__.py
@@ -19,30 +19,6 @@ from .models import SingleBert
 MAX_SEQUENCE_LENGTH = 256
 
 
-# router = fastapi.APIRouter()
-
-
-# @router.get("/test")
-# def test_classification() -> dict[str, typing.Any]:
-#     csv_input_file = io.StringIO(
-#         textwrap.dedent(
-#             """\
-#             id,instruction,statement
-#             P90Ex1,Recopie les deux mots de chaque phrase qui se prononcent de la même façon.,"a. Il a gagné le gros lot à la kermesse des écoles. b. À la fin du film, il y a une bonne surprise. c. Il a garé sa voiture dans le parking, à droite de la nôtre. d. Il m'a invité à venir chez lui. e. Mon oncle a un vélo à vendre."
-#             P90Ex2,Recopie uniquement les phrases avec le verbe avoir.,a. Ce chien a mordu son maître*. b. Je rentre à la maison à midi pour déjeuner en famille. c. On a froid dans ce sous-bois ombragé. d. Il pense toujours qu'il a raison. e. Tu joues à chat avec moi ?
-#             P90Ex3,Recopie et complète ces phrases avec à ou a. Écris avait pour justifier l'emploi de a. Il a (avait) une maison à la campagne.,"a. L'épicerie est ... côté, elle ... une façade jaune. b. L'ordinateur ... remplacé la machine ... écrire. c. Je ne sais pas ... quelle heure il ... décidé d'arriver. d. Elle ... invité tous ses amis ... son anniversaire. e. Maman ... une robe très ... la mode."
-#             P90Ex4,Recopie ces phrases et mets un accent grave sur les a soulignés quand c'est nécessaire.,a. Je n'ai plus rien a lire. b. Ils attendent leurs amis a l'entrée du cinéma. c. Le match a mal commencé pour nous. d. Il a trouvé a manger dans le frigo. e. Ce n'est pas beau a voir.
-#             P90Ex5,Recopie les deux mots de chaque phrase qui se prononcent de la même façon.,a. Mon frère est parti se promener et j'attends son retour. b. Paul et Léa ont un ami qui est hollandais. c. Il est arrivé premier et moi troisième. d. La Suisse est un pays situé entre la France et l'Italie. e. Mon ordinateur est en panne et je vais le faire réparer.
-#             P90Ex6,Recopie uniquement les phrases avec le verbe être.,a. Mon père est très content de mon travail. b. J'ai acheté une bande dessinée et un livre illustré. c. Ce spectacle est formidable. d. J'aime aller à la plage et faire des châteaux de sable. e. C'est bien ici que vous habitez ?
-#             P90Ex7,Recopie et complète ces phrases avec et ou est. Écris était pour justifier l'emploi de est. Elle est (était) contente et souriante.,a. On ... là pour le voir; on lui a apporté des livres ... des sucreries. b. Ce garçon ... serviable ... courageux. c. Le joli singe ... admiré par les visiteurs ... par les gardiens. d. C'... un bijou rare ... précieux. e. Où ... mon jeu vidéo ? J'ai fouillé ma chambre ... le salon.
-#             P90Ex8,Recopie et complète ces phrases avec est ou et.,"a. Tu as fini ton entraînement* ... maintenant, tu vas te reposer. b. Le cerf a entendu les chasseurs ... il s'... sauvé. c. Il ... venu ... il ... reparti. d. Le pirate ... monté sur le bateau ... il a pris l'or ... l'argent. e. Rachid... très gentil... il ... agréable à fréquenter."
-#             P90Ex9,Recopie uniquement les phrases avec le verbe avoir.,"a. On arrivera vers midi si la route est libre. b. Ils ont acheté un bateau à moteur. c. Dans ma famille, on aime se retrouver pour fêter Noël. d. Ces arbres ont l'air de bien pousser. e. Sur le quai, même les goélands ont froid."
-#             """
-#         )
-#     )
-#     return {"df": classify(pd.read_csv(csv_input_file, header=[0])).to_dict(orient="records")}
-
-
 def log(message: str) -> None:
     # @todo Use actual logging
     print(datetime.datetime.now(), message, flush=True)
