81c38e5d32912376ecddcf0bc657c8a7f6156e07
Vincent Jacques <vincent@vincent-jacques.net>
2025-06-19 10:19:14 +0200

    Make end-to-end test more robust

--- frontend/e2e-tests/extraction.cy.ts
+++ frontend/e2e-tests/extraction.cy.ts
@@ -55,8 +55,7 @@ describe('The extraction batch creation page', () => {
     cy.get('h3 span.inProgress:contains("in progress")').should('not.exist')
     cy.get('div.busy').should('have.length', 4)
     cy.get('div.busy').should('have.length', 3)
-    cy.get('div.busy').should('have.length', 2)
-    cy.get('div.busy').should('not.exist')
+    cy.get('div.busy', { timeout: 10000 }).should('not.exist')
     screenshot('extraction-batch-edition-page')
 
     cy.get('a:contains("Download standalone HTML")')
