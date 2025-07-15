3b6fcec38c10a7f9012f855c82b7e01dd0beacb3
Vincent Jacques <vincent@vincent-jacques.net>
2025-07-15 13:22:13 +0200

    Describe spacing variables on examples page

--- frontend/src/AdaptedExerciseExamplesView.vue
+++ frontend/src/AdaptedExerciseExamplesView.vue
@@ -2755,14 +2755,18 @@ onMounted(() => {
   <div style="padding-left: 5px; padding-right: 5px">
     <h1>{{ t('spacing') }}</h1>
     <p>{{ t('changesNotSaved') }}</p>
-    <ul>
-      <li v-for="(variable, key) in spacingVariables" :key="key">
-        <code>{{ key }}</code
-        >:
-        <!-- eslint-disable-next-line @intlify/vue-i18n/no-raw-text -->
-        <input type="number" v-model="spacingVariables[key]" />em
-      </li>
-    </ul>
+    <dl>
+      <template v-for="(variable, key) in spacingVariables" :key="key">
+        <dt>
+          <b>{{ key }}</b>
+        </dt>
+        <dd>
+          <span v-html="t(`variable.${key}`)"></span> :
+          <!-- eslint-disable-next-line @intlify/vue-i18n/no-raw-text -->
+          <input type="number" v-model="spacingVariables[key]" />em
+        </dd>
+      </template>
+    </dl>
 
     <h1>{{ t('examples') }}</h1>
     <template v-for="example in examples" :key="example.title">
@@ -2777,9 +2781,27 @@ onMounted(() => {
 en:
   spacing: Spacing
   changesNotSaved: Changes made here are not saved, and applied only on this page, temporarily.
+  variable:
+    '--extra-horizontal-space-between-words': 'Horizontal space added to the standard space between words'
+    '--optional-extra-horizontal-space-between-letters-in-editable-text-input': 'Horizontal space added between letters when <code>"increaseHorizontalSpace"</code> is <code>true</code> in a <code>"kind": "editableTextInput"</code>'
+    '--vertical-space-between-top-and-instruction': 'Vertical space between the top of the page and the instruction'
+    '--vertical-space-between-instruction-lines': 'Vertical space between lines in the instruction'
+    '--vertical-space-between-instruction-and-statement': 'Vertical space between the instruction and the statement'
+    '--vertical-space-between-statement-lines': 'Vertical space between lines in the statement'
+    '--vertical-space-between-border-and-choices': 'Vertical space between the border and choices in a <code>"kind": "multipleChoicesInput"</code>'
+    '--vertical-space-between-choices-lines': 'Vertical space between lines in the choices of a <code>"kind": "multipleChoicesInput"</code>'
   examples: Examples
 fr:
   spacing: Espacement
   changesNotSaved: Les modifications effectuées ici ne sont pas enregistrées, et ne s'appliquent qu'à cette page, temporairement.
+  variable:
+    '--extra-horizontal-space-between-words': 'Espace horizontal ajouté à l’espace standard entre les mots'
+    '--optional-extra-horizontal-space-between-letters-in-editable-text-input': 'Espace horizontal ajouté entre les lettres quand <code>"increaseHorizontalSpace"</code> est <code>true</code> dans un <code>"kind": "editableTextInput"</code>'
+    '--vertical-space-between-top-and-instruction': 'Espace vertical entre le haut de la page et l’instruction'
+    '--vertical-space-between-instruction-lines': 'Espace vertical entre les lignes de l’instruction'
+    '--vertical-space-between-instruction-and-statement': 'Espace vertical entre l’instruction et l’énoncé'
+    '--vertical-space-between-statement-lines': 'Espace vertical entre les lignes de l’énoncé'
+    '--vertical-space-between-border-and-choices': 'Espace vertical entre le cadre et les choix dans un <code>"kind": "multipleChoicesInput"</code>'
+    '--vertical-space-between-choices-lines': 'Espace vertical entre les lignes des choix dans un <code>"kind": "multipleChoicesInput"</code>'
   examples: Exemples
 </i18n>
