6aa3b917488f6d2e5fd6ce7e885557a66fd10a14
Vincent Jacques <vincent@vincent-jacques.net>
2025-07-16 11:31:33 +0200

    Make virtual keyboard easier to use in textbook index (#91)

--- frontend/src/VirtualEraser.vue
+++ frontend/src/VirtualEraser.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import VirtualKeyboardButton from './VirtualKeyboardButton.vue'
+
 function click() {
   const el = document.activeElement
   if (el instanceof HTMLElement && el.getAttribute('contenteditable') !== null) {
@@ -11,17 +13,12 @@ function click() {
 </script>
 
 <template>
-  <button @pointerdown.prevent="click"><span class="eraser"></span></button>
+  <!-- VirtualEraser.png has been provided by the client in https://github.com/jacquev6/Patty/issues/91 -->
+  <VirtualKeyboardButton @click="click"><img height="32" src="./VirtualEraser.png" /></VirtualKeyboardButton>
 </template>
 
 <style scoped>
-.eraser {
-  display: inline-block;
-  width: 1.5em;
-  height: 0.7em;
-  background: linear-gradient(to right, #ff7e89 0% 50%, #71b2f3 50% 100%);
-  /* Thank you Chat GPT for the CSS magic */
-  clip-path: polygon(10% 0%, 100% 0%, 90% 100%, 0% 100%);
-  transform: rotate(-15deg);
+img {
+  vertical-align: text-bottom;
 }
 </style>
