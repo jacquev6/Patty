9ee737253329ec0ff511d3ad5f26450d885faa26
Vincent Jacques <vincent@vincent-jacques.net>
2025-12-06 08:26:02 +0100

    Load prod DB backup with failing Textbooks (#187)

--- backend/patty/__main__.py
+++ backend/patty/__main__.py
@@ -761,7 +761,7 @@ def backup_database() -> None:
 
 @main.command()
 # @todo Consider always using the most recent backup (and stop changing the default value)
-@click.argument("backup_url", default="s3://jacquev6/patty/prod/backups/patty-backup-20251202-101603.tar.gz")
+@click.argument("backup_url", default="s3://jacquev6/patty/prod/backups/patty-backup-20251206-071602.tar.gz")
 @click.option("--yes", is_flag=True)
 @click.option("--patch-according-to-settings", is_flag=True)
 def restore_database(backup_url: str, yes: bool, patch_according_to_settings: bool) -> None:
