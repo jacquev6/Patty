cc54bee1be1a75a31c1a5cbd48c724b463da242f
Vincent Jacques <vincent@vincent-jacques.net>
2025-12-11 06:14:22 +0100

    Fix logging (#43)

--- backend/patty/adaptation/llm/gemini.py
+++ backend/patty/adaptation/llm/gemini.py
@@ -72,7 +72,7 @@ class GeminiModel(Model):
             )
         except google.genai.errors.ClientError as e:
             if e.code == 429:
-                logs.log("Gemini rate limit exceeded", e)
+                logs.log(f"Gemini rate limit exceeded {e}")
                 raise RetryableError()
             else:
                 raise
