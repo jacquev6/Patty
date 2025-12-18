0412b33fcf292f1bc807ab58c1fdfd6b7765cf69
Vincent Jacques <vincent@vincent-jacques.net>
2025-12-18 15:12:14 +0100

    Load more recent prod DB backup (investigate #201)

--- backend/patty/__main__.py
+++ backend/patty/__main__.py
@@ -763,7 +763,7 @@ def backup_database() -> None:
 
 @main.command()
 # @todo Consider always using the most recent backup (and stop changing the default value)
-@click.argument("backup_url", default="s3://jacquev6/patty/prod/backups/patty-backup-20251211-041602.tar.gz")
+@click.argument("backup_url", default="s3://jacquev6/patty/prod/backups/patty-backup-20251218-131603.tar.gz")
 @click.option("--yes", is_flag=True)
 @click.option("--patch-according-to-settings", is_flag=True)
 def restore_database(backup_url: str, yes: bool, patch_according_to_settings: bool) -> None:
